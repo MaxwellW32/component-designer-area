29710ab122380e06b2fe4cca713d7f040324a247
MaxwellW32 <maxwellwedderburn32@gmail.com>
2025-01-19 08:49:53 -0500

    latest

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,107 @@
-import Nav from "@/components/nav/page";
-import Image from "next/image";
+// import Nav1 from "@/components/nav1/page";
+// import Nav2 from "@/components/nav2/page";
+// import Nav3 from "@/components/nav3/page";
+// import Nav4 from "@/components/nav4/page";
+// import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-      <Nav data={{
-        category: "navbars",
-        menu: [
-          {
-            label: "menu item 1",
-            link: "/",
-            subMenu: [
-              {
-                label: "sub menu item 1",
-                link: "/",
-              }
-            ]
-          },
-          {
-            label: "menu item 2",
-            link: "/",
-          },
-        ]
-      }} />
+      {/* <Nav1
+        data={{
+          "category": "navbars",
+          "mainElProps": {},
+          "styleId": "",
+          "menu": [
+            {
+              "label": "menu item 1",
+              "link": "/",
+              "subMenu": [
+                {
+                  "label": "sub menu item 1",
+                  "link": "/"
+                }
+              ]
+            },
+            {
+              "label": "menu item 2",
+              "link": "/"
+            }
+          ]
+        }}
+      /> */}
+
+      {/* <Nav2
+        data={{
+          "category": "navbars",
+          "mainElProps": {},
+          "styleId": "",
+          "menu": [
+            {
+              "label": "menu item 1",
+              "link": "/",
+              "subMenu": [
+                {
+                  "label": "sub menu item 1",
+                  "link": "/"
+                }
+              ]
+            },
+            {
+              "label": "menu item 2",
+              "link": "/"
+            }
+          ]
+        }}
+      /> */}
+
+      {/* <Nav3
+        data={{
+          "category": "navbars",
+          "mainElProps": {},
+          "styleId": "",
+          "menu": [
+            {
+              "label": "menu item 1",
+              "link": "/",
+              "subMenu": [
+                {
+                  "label": "sub menu item 1",
+                  "link": "/"
+                }
+              ]
+            },
+            {
+              "label": "menu item 2",
+              "link": "/"
+            }
+          ]
+        }}
+      /> */}
+
+      {/* <Nav4
+        data={{
+          "category": "navbars",
+          "mainElProps": {},
+          "styleId": "",
+          "menu": [
+            {
+              "label": "menu item 1",
+              "link": "/",
+              "subMenu": [
+                {
+                  "label": "sub menu item 1",
+                  "link": "/"
+                }
+              ]
+            },
+            {
+              "label": "menu item 2",
+              "link": "/"
+            }
+          ]
+        }}
+      /> */}
     </>
   );
 }
--- types.tsx
+++ types.tsx
@@ -1,5 +1,14 @@
 import { z } from "zod";
 
+//category data type start copy here
+const htmlAttributesSchema = z.object({
+    style: z.record(z.string()).optional(),
+    className: z.string().optional(),
+    id: z.string().optional(),
+});
+export type htmlAttributesType = z.infer<typeof htmlAttributesSchema>
+
+
 const navSubMenuItem = z.object({
     label: z.string(),
     link: z.string(),
@@ -11,7 +20,54 @@ const navMenuItem = z.object({
 })
 export const navBarsSchema = z.object({
     category: z.literal("navbars"),
+    mainElProps: htmlAttributesSchema,
+    styleId: z.string(),
+
     menu: z.array(navMenuItem),
 })
 export type navBarsType = z.infer<typeof navBarsSchema>
 
+
+
+
+
+export const herosSchema = z.object({
+    category: z.literal("heros"),
+    mainElProps: htmlAttributesSchema,
+    styleId: z.string(),
+
+})
+export type herosType = z.infer<typeof herosSchema>
+
+
+
+
+
+export const containersSchema = z.object({
+    category: z.literal("containers"),
+    mainElProps: htmlAttributesSchema,
+    styleId: z.string(),
+
+    children: z.array(z.any()),
+})
+export type containersType = z.infer<typeof containersSchema>
+
+
+
+
+
+export const textElementsSchema = z.object({
+    mainElProps: htmlAttributesSchema,
+    styleId: z.string(),
+
+    category: z.literal("textElements"),
+})
+export type textElementsType = z.infer<typeof textElementsSchema>
+
+
+
+
+
+export const componentDataSchema = z.union([navBarsSchema, herosSchema, containersSchema, textElementsSchema])
+export type componentDataType = z.infer<typeof componentDataSchema>
+//category data type end copy here
